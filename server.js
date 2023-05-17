d179b08b79f547c19669705bf7974a98f8891960
“jollyassjerk” <jollyassjerk@gmail.com>
2023-05-17 17:13:57 +0000

    Added lost customer routes

--- server.js
+++ server.js
@@ -79,31 +79,6 @@ app.get('/products', (req, res)=>{
     dbclient.end;
 })
 
-app.get('/testproductadd', (req, res)=>{
-    
-    res.sendFile(__dirname + '/productPOSTtest.html');
-})
-
-app.get('/testproductget', (req, res)=>{
-    
-    
-
-    dbclient.query(`Select * from products`, (err, result)=>{
-        if(!err){            
-            var html = "<html>";
-
-            result.rows.forEach(row => {
-
-                html += `<p>RECORD: ${row.id} | Name: ${row.name} | Description: ${row.description}  | Image: <img src='images/${row.image_path}'> |</p><br>`;
-            });
-
-            html += "</html>";
-            
-            res.send(html);
-        }
-    });
-    dbclient.end;    
-})
 
 app.post('/product', upload.single('image'), (req, res)=>{    
 
@@ -137,7 +112,7 @@ app.put('/product/:id', (req, res)=>{
     let description = req.body.description;
     let id = req.params.id;
     dbclient.query(
-        `UPDATE products(name, price, image, description)
+        `UPDATE products
          SET name = '${name}', price = ${price}, 
          image = '${image}', description = '${description}'
          WHERE id = ${id}`,
@@ -163,6 +138,32 @@ app.delete('/product/:id', (req, res)=>{
         }
     });
     dbclient.end;
+})
+
+app.get('/testproductadd', (req, res)=>{
+    
+    res.sendFile(__dirname + '/productPOSTtest.html');
+})
+
+app.get('/testproductget', (req, res)=>{
+    
+    
+
+    dbclient.query(`Select * from products`, (err, result)=>{
+        if(!err){            
+            var html = "<html>";
+
+            result.rows.forEach(row => {
+
+                html += `<p>RECORD: ${row.id} | Name: ${row.name} | Description: ${row.description}  | Image: <img src='images/${row.image_path}'> |</p><br>`;
+            });
+
+            html += "</html>";
+            
+            res.send(html);
+        }
+    });
+    dbclient.end;    
 })
 
 
@@ -291,113 +292,173 @@ app.post('/users', async (req, res) => {
 
 })
 
+
 app.post('/users/reset/:email', (req, res) => {
 
-try {
-    
-    const result = dbclient.query(`SELECT * FROM users 
-    WHERE email = '${req.params.email}'`, (err, result)=>{
+    try {
+        
+        const result = dbclient.query(`SELECT * FROM users 
+        WHERE email = '${req.params.email}'`, (err, result)=>{
 
-        if(!err){
+            if(!err){
 
-            if (result.rows.length === 0) {
-                return res.status(404).json({ message: 'Email not found' });
-            } else {
+                if (result.rows.length === 0) {
+                    return res.status(404).json({ message: 'Email not found' });
+                } else {
 
-                const resetToken = crypto.randomBytes(32).toString('hex');
+                    const resetToken = crypto.randomBytes(32).toString('hex');
 
-                dbclient.query(`INSERT INTO password_reset_tokens (user_id, token, expires_at) 
-                VALUES ($1, $2, NOW() + INTERVAL \'1 day\')`, 
-                    [result.rows[0].id, resetToken], (err, result)=>{
-                    if(!err){
-                        return res.status(200).json(
-                            { message: 'Password reset token generated', token: resetToken }
-                        );
-                        //We shouldn't actually expose this to the front end,
-                        // but instead the backend should send the mail directly
-                        //if we have time we can look into thise
-                    }
-                })
+                    dbclient.query(`INSERT INTO password_reset_tokens (user_id, token, expires_at) 
+                    VALUES ($1, $2, NOW() + INTERVAL \'1 day\')`, 
+                        [result.rows[0].id, resetToken], (err, result)=>{
+                        if(!err){
+                            return res.status(200).json(
+                                { message: 'Password reset token generated', token: resetToken }
+                            );
+                            //We shouldn't actually expose this to the front end,
+                            // but instead the backend should send the mail directly
+                            //if we have time we can look into thise
+                        }
+                    })
+
+                }
 
             }
+            
+        });
 
-        }
-        
-    });
-
-} catch (error) {
-    console.error('Error generating reset token:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-}
+    } catch (error) {
+        console.error('Error generating reset token:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
 
 });
 
 
 app.put('/users/reset/:token', (req, res) => {
 
-try {      
-    dbclient.query(`SELECT * FROM password_reset_tokens 
-    WHERE expires_at > NOW()
-    AND token = '${req.params.token}'`, (err, result)=>{
-        if(!err){
+    try {      
+        dbclient.query(`SELECT * FROM password_reset_tokens 
+        WHERE expires_at > NOW()
+        AND token = '${req.params.token}'`, (err, result)=>{
+            if(!err){
 
-            if (result.rows.length === 0) {
-                return res.status(404).json({ message: 'Valid token not found' });
-            } else {
+                if (result.rows.length === 0) {
+                    return res.status(404).json({ message: 'Valid token not found' });
+                } else {
 
-                const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-                var userid = result.rows[0]["user_id"];
-            
-                dbclient.query(`UPDATE users SET password = '${hashedPassword}' 
-                    WHERE id = ${userid}`, (err, result)=>{
-                    
-                    if(!err){
+                    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+                    var userid = result.rows[0]["user_id"];
+                
+                    dbclient.query(`UPDATE users SET password = '${hashedPassword}' 
+                        WHERE id = ${userid}`, (err, result)=>{
+                        
+                        if(!err){
 
-                        dbclient.query(`DELETE FROM  password_reset_tokens 
-                        WHERE user_id = ${userid}`, (err, result)=>{
-                            return res.status(200).json(
-                                { message: 'Password updated' }
-                            );
-                            });
-                    }
-                })
+                            dbclient.query(`DELETE FROM  password_reset_tokens 
+                            WHERE user_id = ${userid}`, (err, result)=>{
+                                return res.status(200).json(
+                                    { message: 'Password updated' }
+                                );
+                                });
+                        }
+                    })
 
-            }
+                }
 
-        }            
-    });  
-} catch (error) {
-    console.error('Error generating reset token:', error);
-    return res.status(500).json({ message: 'Internal server error' });
-}
+            }            
+        });  
+    } catch (error) {
+        console.error('Error generating reset token:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
 
 });
 
 
 app.post('/login', (req, res) => {
-    
-try {
-                
-    dbclient.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, (err, result)=>{
-    
-    if (result.rowCount === 0) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-        } else {
-        const user = result.rows[0];
-        const isValidPassword = bcrypt.compareSync(req.body.password, user.password);
-        if (!isValidPassword) {
+        
+    try {
+                    
+        dbclient.query(`SELECT * FROM users WHERE email = '${req.body.email}'`, (err, result)=>{
+        
+        if (result.rowCount === 0) {
             return res.status(401).json({ message: 'Invalid email or password' });
-        } else {
-            req.session.userId = user.id;
-            return res.status(200).json({ message: 'Login successful' });
-        }
+            } else {
+            const user = result.rows[0];
+            const isValidPassword = bcrypt.compareSync(req.body.password, user.password);
+            if (!isValidPassword) {
+                return res.status(401).json({ message: 'Invalid email or password' });
+            } else {
+                req.session.userId = user.id;
+                return res.status(200).json({ message: 'Login successful' });
+            }
 
-        }
-    });
+            }
+        });
 
-} catch (error) {
-    console.error('Error during login:', error);
-    return res.status(500).json({ message: 'Server error' });
-}
+    } catch (error) {
+        console.error('Error during login:', error);
+        return res.status(500).json({ message: 'Server error' });
+    }
 
 });
+
+
+
+app.get('/customers', (req, res)=>{
+    dbclient.query(`Select * from customers`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    dbclient.end;
+})
+
+
+app.post('/customers', upload.single('image'), (req, res)=>{    
+
+      dbclient.query(
+        `INSERT INTO customers(
+                name
+                , email
+                , address
+                , phone
+            )
+            VALUES(
+                '${req.body.name}'
+                ,'${req.body.email}'
+                ,${req.body.address}
+                ,'${req.file.phone}'
+            )`, (err, result)=>{
+            if(!err){
+                console.log("success!");
+                res.send(result);
+            } else {
+                console.log(err);
+            }
+        });
+    dbclient.end;
+})
+
+app.put('/customer/:id', (req, res)=>{    
+    let name = req.body.name;
+    let email = req.body.email;
+    let address = req.body.address;
+    let phone = req.body.phone;
+    let id = req.params.id;
+    dbclient.query(
+        `UPDATE customers
+         SET name = '${name}', email = ${email}, 
+         address = '${address}', phone = '${phone}'
+         WHERE id = ${id}`,
+        (err, result)=>{
+            if(!err){
+                console.log("success!");
+                res.send(result);
+            } else {
+                console.log(err);
+            }
+        });
+    dbclient.end;
+})
